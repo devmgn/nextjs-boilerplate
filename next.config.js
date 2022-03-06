bd0e23f50ea39a67317dd7b21b99061c80e4011f
JUNICHI IMAI <dev@mgnstudio.com>
2022-03-06 20:20:28 +0900

    add next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withPlugins = require('next-compose-plugins');
+const bundleAnalyzer = require('@next/bundle-analyzer');
+// @ts-ignore
+// TODO: this plugin is not fully support nextjs v12
+const optimizedImages = require('next-optimized-images');
 const appSettings = require('./appSettings.json');
 
 /**
@@ -15,10 +19,36 @@ const nextConfig = {
     styledComponents: true,
     reactRemoveProperties: true,
   },
+  images: {
+    disableStaticImages: true,
+  },
   webpack: (config) => {
     Object.assign(config.externals, appSettings);
     return config;
   },
 };
 
-module.exports = withPlugins([nextConfig]);
+const bundleAnalyzerConfig = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const optimizedImagesConfig = {
+  inlineImageLimit: 1,
+  optimizeImagesInDev: true,
+  mozjpeg: {
+    quality: 10,
+  },
+  pngquant: {},
+  svgo: {
+    name: 'preset-default',
+    params: {
+      overrides: {
+        removeAttrs: {
+          params: { attrs: ['data.*'] },
+        },
+      },
+    },
+  },
+};
+
+module.exports = withPlugins([bundleAnalyzerConfig, [optimizedImages, optimizedImagesConfig]], nextConfig);
