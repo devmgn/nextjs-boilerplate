ed990269bd7ec85d04ec7b22a9e61f2d2741f079
JUNICHI IMAI <dev@mgnstudio.com>
2022-03-06 20:55:45 +0900

    add styled-breakpoints

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { createTheme } from 'styled-breakpoints';
 import Layout from '@/components/layouts';
 import GlobalStyle from '@/components/foundation/globalStyle';
 
+const theme = createTheme({
+  xs: '0px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px',
+  xxl: '1400px',
+});
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Layout>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 };
 
