9940abe554abb5df5c7a067b1abc85e7132a082c
JUNICHI IMAI <dev@mgnstudio.com>
2024-01-01 19:41:43 +0900

    fix api url

--- src/components/Sandbox/FetchMock.tsx
+++ src/components/Sandbox/FetchMock.tsx
@@ -1,23 +1,15 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { DiscList, ListItem } from '@yamada-ui/react';
-import { shuffle } from 'lodash-es';
+import { Text } from '@yamada-ui/react';
 import { apiClient } from '@/utils';
 
 export const FetchMock: React.FC<{ queryKey: number }> = ({ queryKey }) => {
   const { data } = useSuspenseQuery({
     queryKey: [queryKey],
-    queryFn: async () => {
-      const response = await apiClient.get<{ name: string }[]>(
-        'https://jsonplaceholder.typicode.com/users',
-      );
-
-      return queryKey > 0 ? shuffle(response) : response;
-    },
+    queryFn: () =>
+      apiClient.get<string>('https://hub.dummyapis.com/delay', {
+        params: { seconds: 2 },
+      }),
   });
 
-  return (
-    <DiscList>
-      {data?.map((d) => <ListItem key={d.name}>{d.name}</ListItem>)}
-    </DiscList>
-  );
+  return <Text>{data && data}</Text>;
 };
--- src/components/Sandbox/index.tsx
+++ src/components/Sandbox/index.tsx
@@ -1,13 +1,19 @@
 'use client';
 
 import { Suspense, useState, useTransition } from 'react';
+import dynamic from 'next/dynamic';
 import { Button, Flex, Loading } from '@yamada-ui/react';
 import { uniqueId } from 'lodash-es';
-import { FetchMock } from './FetchMock';
+
+const FetchMock = dynamic(
+  () => import('./FetchMock').then((m) => m.FetchMock),
+  { ssr: false },
+);
 
 export const Sandbox = () => {
   const [queryKey, setQueryKey] = useState<number>(0);
   const [isPending, startTransition] = useTransition();
+
   const updateQueryKey = () => {
     startTransition(() => setQueryKey(Number(uniqueId())));
   };
@@ -15,14 +21,11 @@ export const Sandbox = () => {
   return (
     <>
       <Flex gap="2" alignItems="center">
-        <Button onClick={updateQueryKey} _ripple={undefined}>
-          Update Key
-        </Button>
+        <Button onClick={updateQueryKey}>Update Key</Button>
         {isPending && <Loading size="2xl" />}
       </Flex>
-      <Suspense fallback="loading...">
-        <FetchMock queryKey={queryKey} />
-      </Suspense>
+      <FetchMock queryKey={queryKey} />
+      <Suspense fallback="loading..." />
     </>
   );
 };
