f22ed5ab25f037a1b5da5583233c1e9ddc613757
JUNICHI IMAI <dev@mgnstudio.com>
2024-02-12 20:06:47 +0900

    fix layout

--- src/app/(sandbox)/tanstack-query/layout.tsx
+++ src/app/(sandbox)/tanstack-query/layout.tsx
@@ -2,8 +2,8 @@ const TanstackQueryLayout: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   return (
-    <main className="prose mx-auto max-w-3xl p-4 prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-ul:p-0">
-      <h1>Tanstack Query Sandbox</h1>
+    <main className="container prose prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-ul:list-none prose-ul:p-0">
+      <h1 className="sm:text-4xl">Tanstack Query Sandbox</h1>
       {children}
     </main>
   );
