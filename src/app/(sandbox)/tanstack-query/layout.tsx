25380c41ba5d785f866340951ab036ec205cc19f
JUNICHI IMAI <dev@mgnstudio.com>
2024-02-17 17:25:45 +0900

    update

--- src/app/(sandbox)/tanstack-query/layout.tsx
+++ src/app/(sandbox)/tanstack-query/layout.tsx
@@ -2,7 +2,7 @@ const TanstackQueryLayout: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   return (
-    <main className="container prose prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-ul:list-none prose-ul:p-0">
+    <main className="prose prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-ul:list-none prose-ul:p-0 container">
       <h1 className="sm:text-4xl">Tanstack Query Sandbox</h1>
       {children}
     </main>
