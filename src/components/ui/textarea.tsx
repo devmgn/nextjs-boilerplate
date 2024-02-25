07619c649ac7fb70da928bd847f2ea8f33d67461
JUNICHI IMAI <dev@mgnstudio.com>
2024-02-25 17:26:55 +0900

    fix

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { cn } from '@/utils';
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
