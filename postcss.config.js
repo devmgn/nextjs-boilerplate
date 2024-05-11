f374b1c99c85ce65a56ecc6bfca5e3fd981d4ff9
JUNICHI IMAI <dev@mgnstudio.com>
2024-05-11 10:54:26 +0900

    remove unnecessary postcss modules

--- postcss.config.js
+++ postcss.config.js
@@ -5,15 +5,7 @@
 
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': false,
-      },
-      autoprefixer: {},
-    },
+    autoprefixer: {},
   },
 };
