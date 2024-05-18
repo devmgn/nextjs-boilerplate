82e7627ff3b2f901388c4288e5914ce9ab1b8d2e
JUNICHI IMAI <dev@mgnstudio.com>
2024-05-18 10:31:41 +0900

    fix: useIsComposing

--- src/hooks/useIsComposing/useIsCompsing.test.ts
+++ src/hooks/useIsComposing/useIsCompsing.test.ts
@@ -26,7 +26,7 @@ describe('useIsComposing', () => {
 
       act(() => {
         const event = new Event(eventName);
-        window.dispatchEvent(event);
+        document.dispatchEvent(event);
       });
 
       expect(result.current).toBe(expected);
