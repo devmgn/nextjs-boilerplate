4284726c6a5bc65b09c8783fe41a615cd4dec57c
JUNICHI IMAI <dev@mgnstudio.com>
2024-06-29 15:06:59 +0900

    left-hook

--- .lintstagedrc.mjs
+++
@@ -1,8 +0,0 @@
-import { relative } from 'node:path';
-
-const buildEslintCommand = (filenames) =>
-  `biome format ${filenames.map((f) => relative(process.cwd(), f)).join(' ')}`;
-
-export default {
-  '*.[jt]s?(x)': ['bash -c "yarn tsc"', buildEslintCommand, 'yarn test'],
-};
