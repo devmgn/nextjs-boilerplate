6a2c67898cbe749a947c48b2bbc4e5f22695f11c
JUNICHI IMAI <dev@mgnstudio.com>
2024-07-08 07:53:36 +0900

    revert hook-form

---
+++ src/app/(sandbox)/react-hook-form/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { useLoginForm } from './useLoginForm';
+
+export default function ReactHookForm() {
+  const {
+    submit,
+    form: {
+      register,
+      formState: { errors },
+    },
+  } = useLoginForm();
+
+  return (
+    <main className="container prose">
+      <h1 className="sm:text-4xl">react-hook-form example</h1>
+      <form onSubmit={submit}>
+        <div className="space-y-6">
+          <div className="space-y-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" {...register('email')} />
+            {errors.email?.message && (
+              <p className="text-sm text-red-400">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="space-y-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" {...register('password')} />
+            {errors.password?.message && (
+              <p className="text-sm text-red-400">{errors.password.message}</p>
+            )}
+          </div>
+          <Button type="submit">送信</Button>
+        </div>
+      </form>
+    </main>
+  );
+}
---
+++ src/app/(sandbox)/react-hook-form/useLoginForm/schema.ts
@@ -0,0 +1,8 @@
+import * as v from 'valibot';
+
+export const LoginSchema = v.object({
+  email: v.pipe(v.string(), v.maxLength(8), v.email()),
+  password: v.pipe(v.string(), v.minLength(8), v.maxLength(255)),
+});
+
+export type LoginRequest = v.InferInput<typeof LoginSchema>;
