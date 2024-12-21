a95da06a1e39467bcfddcb490871ae361a872b46
JUNICHI IMAI <dev@mgnstudio.com>
2024-12-21 11:19:26 +0900

    rename paraseEnv

--- src/utils/parseEnv/safeParseEnv.test.ts
+++ src/utils/parseEnv/safeParseEnv.test.ts
@@ -1,6 +1,6 @@
-import { safeParseEnv } from ".";
+import { parseEnv } from ".";
 
-describe("safeParseEnv", () => {
+describe("parseEnv", () => {
   beforeEach(() => {
     vi.stubGlobal("process", {
       env: {
@@ -18,68 +18,66 @@ describe("safeParseEnv", () => {
 
   describe("文字列の取得", () => {
     it("オプションなしで呼び出されたとき、文字列値を返すこと", () => {
-      expect(safeParseEnv(process.env.testString)).toBe("test");
+      expect(parseEnv(process.env.testString)).toBe("test");
     });
 
     it("環境変数が定義されていないとき、エラーをスローすること", () => {
-      expect(() => safeParseEnv(process.env.undefinedEnv)).toThrow();
+      expect(() => parseEnv(process.env.undefinedEnv)).toThrow();
     });
   });
 
   describe("数値の取得", () => {
     it("asNumber: trueで呼び出されたとき、数値を返すこと", () => {
-      expect(safeParseEnv(process.env.testNumber, { asNumber: true })).toBe(
-        123,
-      );
+      expect(parseEnv(process.env.testNumber, { asNumber: true })).toBe(123);
     });
 
     it("asNumber: trueで呼び出され、値が有効な数値でないとき、エラーをスローすること", () => {
       expect(() =>
-        safeParseEnv(process.env.testString, { asNumber: true }),
+        parseEnv(process.env.testString, { asNumber: true }),
       ).toThrow();
     });
 
     it("環境変数が定義されていないとき、エラーをスローすること", () => {
       expect(() =>
-        safeParseEnv(process.env.undefinedEnv, { asNumber: true }),
+        parseEnv(process.env.undefinedEnv, { asNumber: true }),
       ).toThrow();
     });
   });
 
   describe("真偽値の取得", () => {
     it('asBoolean: trueで呼び出され、値が"true"のとき、trueを返すこと', () => {
-      expect(
-        safeParseEnv(process.env.testBooleanTrue, { asBoolean: true }),
-      ).toBe(true);
+      expect(parseEnv(process.env.testBooleanTrue, { asBoolean: true })).toBe(
+        true,
+      );
     });
 
     it('asBoolean: trueで呼び出され、値が"false"のとき、falseを返すこと', () => {
-      expect(
-        safeParseEnv(process.env.testBooleanFalse, { asBoolean: true }),
-      ).toBe(false);
+      expect(parseEnv(process.env.testBooleanFalse, { asBoolean: true })).toBe(
+        false,
+      );
     });
 
     it("asBoolean: trueで呼び出され、値が有効な真偽値でないとき、エラーをスローすること", () => {
       expect(() =>
-        safeParseEnv(process.env.testString, { asBoolean: true }),
+        parseEnv(process.env.testString, { asBoolean: true }),
       ).toThrow();
     });
 
     it("環境変数が定義されていないとき、エラーをスローすること", () => {
       expect(() =>
-        safeParseEnv(process.env.undefinedEnv, { asBoolean: true }),
+        parseEnv(process.env.undefinedEnv, { asBoolean: true }),
       ).toThrow();
     });
   });
 
   describe("nullの取得", () => {
     it('値が"null"のとき、nullを返すこと', () => {
-      expect(safeParseEnv(process.env.testString, { asNull: true })).toBe(null);
+      expect(parseEnv(process.env.testString, { asNull: true })).toBe(null);
     });
 
     it("環境変数が定義されていないとき、エラーをスローすること", () => {
       expect(() =>
-        safeParseEnv(process.env.undefinedEnv, { asNull: true }),
+        parseEnv(process.env.undefinedEnv, { asNull: true }),
       ).toThrow();
     });
   });
