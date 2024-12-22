913c226841296d4666ff55dd17a2b78e9a061de9
JUNICHI IMAI <dev@mgnstudio.com>
2024-12-22 18:30:26 +0900

    add web vitals log

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import { ENV } from "../config/env";
+import { WebVitalsReporter } from "../lib/WebVitalsReporter";
 import { RootProvider } from "../providers/RootProvider";
+import { isProduction } from "../utils/nodeEnv";
 import { parseEnv } from "../utils/parseEnv";
 import "./globals.css";
 
@@ -20,6 +22,7 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="ja">
       <body>
+        {!isProduction && <WebVitalsReporter />}
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
