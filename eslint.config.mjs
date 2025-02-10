64ac1822603bd51a61f3b32c27c6cadafb384a32
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2025-02-10 19:02:39 +0900

    Feature/fix eslint (#1603)
    * update: eslint.config for vitest
    
    * fix: storyboo, tanstackQuery, react-compoiler
    
    * fix: zod

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
 import { FlatCompat } from "@eslint/eslintrc";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
+import vitest from "@vitest/eslint-plugin";
 import reactCompiler from "eslint-plugin-react-compiler";
+import storybook from "eslint-plugin-storybook";
 
 const compat = new FlatCompat();
 
+/** @type {import('eslint').Linter.Config[]} */
 const config = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:@tanstack/eslint-plugin-query/recommended",
-  ),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    plugins: {
-      "react-compiler": reactCompiler,
-    },
     rules: {
       "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/no-explicit-any": "off",
@@ -20,24 +17,20 @@ const config = [
       "lines-around-directive": "error",
       "no-multiple-empty-lines": "error",
       "prefer-template": "error",
-      "react-compiler/react-compiler": "error",
       "react/jsx-curly-brace-presence": "error",
       "react/jsx-sort-props": "error",
     },
   },
-  ...compat.extends("plugin:storybook/recommended").map((config) => ({
-    ...config,
-    files: ["**/*.stories.ts?(x)"],
-  })),
-  ...compat.extends("plugin:@vitest/legacy-recommended").map((config) => ({
-    ...config,
-    files: ["**/?(*.)+(spec|test).[tj]s?(x)"],
-  })),
+  reactCompiler.configs.recommended,
+  ...tanstackQuery.configs["flat/recommended"],
+  ...storybook.configs["flat/recommended"],
   {
     files: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+    plugins: { vitest },
     rules: {
-      "@vitest/consistent-test-it": [2, { fn: "it" }],
-      "@vitest/require-top-level-describe": ["error"],
+      ...vitest.configs.recommended.rules,
+      "vitest/consistent-test-it": ["error", { fn: "it" }],
+      "vitest/require-top-level-describe": "error",
     },
   },
   { ignores: ["src/api/openapi"] },
