96c5e5af72d405566d02cb51a734b3cc52ddde0c
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2025-02-17 19:39:46 +0900

    createMiddlewareChainのPromise化

--- src/lib/middlewares/utils/createMiddlewareChain/index.ts
+++ src/lib/middlewares/utils/createMiddlewareChain/index.ts
@@ -1,16 +1,15 @@
 import type { NextResponse } from "next/server";
 import type { Middleware } from "../../type";
-
 /**
  * middleware 関数を順番に実行するミドルウェアを作成する
  */
 export const createMiddlewareChain =
   (...middlewares: Middleware[]): Middleware =>
-  (req, event, next) => {
-    const execMiddleware = (index = 0): NextResponse =>
+  async (req, event, next) => {
+    const execMiddleware = (index = 0): Promise<NextResponse> =>
       middlewares[index]
         ? middlewares[index](req, event, () => execMiddleware(index + 1))
         : next();
 
-    return execMiddleware();
+    return await execMiddleware();
   };
