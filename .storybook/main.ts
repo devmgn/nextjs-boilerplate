92a0ea67304051c70f7732e343e90330d53f2212
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2025-05-17 07:49:03 +0900

    Remove @storybook/addon-links dependency (#1907)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/nextjs-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
     "@storybook/addon-vitest",
