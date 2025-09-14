d36f7b122c97c0b4e77d5dc6812760364f25b73b
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2025-09-15 08:06:00 +0900

    fix: Knipの設定を修正し、MSWバイナリの警告を解消 (#2269)
    * fix: knip
    
    * fix
    
    * fix

---
+++ src/app/(sandbox)/dummy/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { FaceIcon } from "@radix-ui/react-icons";
+import { isArray } from "es-toolkit/compat";
+import { Card } from "../../../components/Card";
+import { SvgIcon } from "../../../components/SvgIcon";
+import { useDebouncedCallback } from "../../../hooks/useDebouncedCallback";
+import { useDisclosure } from "../../../hooks/useDisclosure";
+import { useIsComposing } from "../../../hooks/useIsComposing";
+import { asyncDebounce } from "../../../utils/asyncDebounce";
+import { createCustomEvent } from "../../../utils/createCustomEvent";
+import { isKeyOf } from "../../../utils/isKeyOf";
+import { isServer } from "../../../utils/isServer";
+import { isValueOf } from "../../../utils/isValueOf";
+import { isDevelopment } from "../../../utils/nodeEnv";
+import type {} from "type-fest";
+
+export default function Page() {
+  useDisclosure();
+  useDebouncedCallback(() => {
+    // noop
+  }, 1000);
+  useIsComposing();
+  asyncDebounce(() => {
+    // noop
+  }, 1000);
+  createCustomEvent("" as keyof GlobalEventHandlersEventMap);
+  const _isServer = isServer;
+  const _isDevelopment = isDevelopment;
+  const _isKeyOf = isKeyOf({}, "");
+  const _isValueOf = isValueOf({}, "");
+  const _isArray = isArray([]);
+
+  return (
+    <>
+      <SvgIcon icon={FaceIcon} label="" />
+      <Card />
+    </>
+  );
+}
