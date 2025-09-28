e3883a8e7be4eeae8e50d10b6cf7dc4e72fbb814
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2025-09-28 11:25:28 +0900

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,14 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      include: ["src/**"],
-      exclude: ["**/*.d.ts", "src/mocks/**", "src/api/**", "**/*.stories.tsx"],
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "**/*.d.ts",
+        "src/**/*.stories.tsx",
+        "src/{api,mocks}/**",
+        "src/{instrumentation-client,instrumentation,middleware}.ts",
+        "src/app/**/{default,error,forbidden,global-error,global-not-found,layout,loading,not-found,page,route,template,unauthorized}.{ts,tsx}",
+      ],
       thresholds: {
         lines: 80,
         functions: 80,
