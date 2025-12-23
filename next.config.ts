4601a5ee07572a77a96ccd28da684773bc7a6561
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2025-12-23 18:08:32 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import withBundleAnalyzer from "@next/bundle-analyzer";
+
 import type { NextConfig } from "next";
 
 const nextConfig = {
@@ -16,6 +16,4 @@ const nextConfig = {
 } satisfies NextConfig;
 
 // biome-ignore lint/style/noDefaultExport: using default export
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-})(nextConfig);
+export default nextConfig;
