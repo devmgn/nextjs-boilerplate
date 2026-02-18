ab2a652da1d9649493eaa735fbce0239696656fb
JUNICHI IMAI <7300018+devmgn@users.noreply.github.com>
2026-02-18 16:50:04 +0900

    fix

--- src/utils/runtime/runtime.ts
+++ src/utils/runtime/runtime.ts
@@ -1,4 +1,5 @@
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const isProduction = process.env.NODE_ENV === "production";
-// oxlint-disable-next-line typescript/no-unnecessary-condition
-export const isServer = globalThis.window === undefined;
+
+// oxlint-disable-next-line unicorn/prefer-global-this
+export const isServer = typeof window === "undefined";
